e7a406bbe54f55e59be43aa01bfbe11bd8a18ed2
altrevis <leo.benazeth@gmail.com>
2023-05-03 16:16:35 +0200

    meta referencement

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
   <!-- Required meta tags -->
 
   <meta charset="utf-8">
+  <meta name="keywords" content="blizzard, wow, world of warcraft, diablo, starcraft, overwatch">
 
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
